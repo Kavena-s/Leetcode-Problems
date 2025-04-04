2c41143e397e3db2a3338c3668c953eb892c087a
KAVENA S <spkavena@gmail.com>
2025-04-04 09:30:01 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.16%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n<=1)
+            return n;
+        int num1=1,num2=1;
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            ans=num1+num2;
+            num1=num2;
+            num2=ans;
+        }
+        return ans;
+    }
+}
