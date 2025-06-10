3548d73bc4a3753eb9157cfcb786c39c3e982eae
KAVENA S <spkavena@gmail.com>
2025-06-10 06:39:30 +0530

    Time: 1 ms (81.82%), Space: 43.1 MB (12.74%) - LeetHub

--- 2093-check-if-string-is-a-prefix-of-array/2093-check-if-string-is-a-prefix-of-array.java
+++ 2093-check-if-string-is-a-prefix-of-array/2093-check-if-string-is-a-prefix-of-array.java
@@ -1,15 +1,10 @@
 class Solution {
     public boolean isPrefixString(String s, String[] words) {
         int len = 0;
+        String str = "";
         for(int i=0 ; i<words.length ; i++){
-            len += words[i].length();
-            if(len == s.length()){
-                String str = "";
-                for(int j=0 ; j<=i ; j++){
-                    str = str.concat(words[j]);
-                    if(str.equals(s)) return true;
-                }
-            }
+            str = str.concat(words[i]);
+            if(str.equals(s)) return true;
         }
         return false;
     }
