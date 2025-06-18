c984d79b514d2a1c91800ee9ead44b02d15affed
KAVENA S <spkavena@gmail.com>
2025-06-18 15:07:06 +0530

    Time: 6 ms (26.14%), Space: 41.9 MB (53.34%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -4,16 +4,13 @@ class Solution {
         for(int i=0 ; i<nums.length ; i++){
             int first = i;
             int last =i;
-            int temp = 1;
             for(int j=i+1;j<nums.length;j++){
                 if(nums[j-1]+1!=nums[j]) {
                     last = j-1;
-                    temp = j;
                     break;
                 }
                 if(j == nums.length - 1) { 
                     last = j;
-                    temp = nums.length;
                 }   
             }
             if(first == last)list.add(String.valueOf(nums[i]));
