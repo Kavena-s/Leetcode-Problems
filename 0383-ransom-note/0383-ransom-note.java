efd5a759c36d2a9bfdb6c030c7e840b750eee984
KAVENA S <spkavena@gmail.com>
2025-07-04 09:52:44 +0530

    Time: 1 ms (99.43%), Space: 44.4 MB (98.65%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        for(char c : magazine.toCharArray())
+            arr[c - 'a'] += 1;
+        for(char c : ransomNote.toCharArray()){
+            if(arr[c - 'a'] == 0)return false;
+            else arr[c - 'a'] -= 1;
+        }
+        return true;
+    }
+}
