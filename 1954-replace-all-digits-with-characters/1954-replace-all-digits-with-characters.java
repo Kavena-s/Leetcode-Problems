caabeff5fe524d5fab06e7c49750ebf000e511ef
KAVENA S <spkavena@gmail.com>
2025-08-11 22:53:42 +0530

    Time: 1 ms (51.72%), Space: 41.6 MB (60.06%) - LeetHub

---
+++ 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String replaceDigits(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0 ; i<s.length() ; i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                int num = c - '0';
+                sb.append((char)(num + s.charAt(i-1)));
+            }
+            else{
+                sb.append(c);;
+            }
+        }
+        return sb.toString();
+    }
+}
