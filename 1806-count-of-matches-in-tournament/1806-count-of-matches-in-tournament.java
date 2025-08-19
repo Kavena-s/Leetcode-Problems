9d931cf87b88682f0d3dcb21d0a3392351fd2e5c
KAVENA S <spkavena@gmail.com>
2025-08-19 14:59:03 +0530

    Time: 0 ms (100%), Space: 40.8 MB (29.64%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        // int match = n/2;
+        // if(n % 2 == 0) n /= 2;
+        // else n = (n-1)/2 + 1;
+        // int sum = match;
+        // while(n > 1){
+        //     match = n / 2;
+        //     if(n % 2 == 0)n/= 2;
+        //     else n = (n - 1)/2 + 1;
+        //     sum += match;
+        // }
+        // return sum ;
+        return n-1;
+    }
+}
