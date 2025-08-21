5f7e96936f143dcd3f790787fd7125c9312c43b9
KAVENA S <spkavena@gmail.com>
2025-08-21 13:35:17 +0530

    Time: 5 ms (68.61%), Space: 42.3 MB (21.34%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,20 +1,15 @@
 class Solution {
     public int longestPalindrome(String s) {
-        int arr[] = new int[52];
+        HashSet<Character> hash = new HashSet();
+        int pairs = 0;
         for(char c : s.toCharArray()){
-            if(Character.isLowerCase(c))arr[c-'a']++;
-            else arr[c - 'A' + 26]++;
-        }
-        int pal = 0;
-        int odd = 0;
-        for(int i=0 ; i<arr.length ; i++){
-            if(arr[i]%2 == 0) pal += arr[i];
-            else{
-                odd++;
-                pal+=arr[i]-1;
+            if(hash.contains(c)){
+                pairs++;
+                hash.remove(c);
             }
+            else hash.add(c);
         }
-        if(odd==0) return pal;
-        return pal+1;
+        if(hash.isEmpty()) return 2 * pairs;
+        return 2 * pairs + 1;
     }
 }
